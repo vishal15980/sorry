3b19be1019117c158e3c5985db9b2bb85b2e432b
Vishal patil <vishalpatil120923@gmail.com>
2026-04-12 15:37:30 +0530

    Refactor envelope interaction and add animation
    Refactor envelope open/close functions and add hearts animation.

--- script.js
+++ script.js
@@ -3,20 +3,32 @@ $(document).ready(function () {
   var btn_open = $("#open");
   var btn_reset = $("#reset");
 
+  // Open when clicking envelope
   envelope.click(function () {
-    open();
-  });
-  btn_open.click(function () {
-    open();
-  });
-  btn_reset.click(function () {
-    close();
+    openEnvelope();
   });
 
-  function open() {
+  // Open button
+  btn_open.click(function () {
+    openEnvelope();
+  });
+
+  // Close button
+  btn_reset.click(function () {
+    closeEnvelope();
+  });
+
+  function openEnvelope() {
     envelope.addClass("open").removeClass("close");
+
+    // Trigger hearts animation (optional class)
+    $(".heart").addClass("active");
   }
-  function close() {
+
+  function closeEnvelope() {
     envelope.addClass("close").removeClass("open");
+
+    // Reset hearts animation
+    $(".heart").removeClass("active");
   }
 });
