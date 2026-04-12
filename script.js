945f0bb00992e676e4331b10848b1b98f489e649
Vishal patil <vishalpatil120923@gmail.com>
2026-04-12 16:00:43 +0530

    Refactor envelope open/close logic into functions

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 $(document).ready(function () {
-
   var envelope = $("#envelope");
   var btn_open = $("#open");
   var btn_reset = $("#reset");
 
-  // Open on button click
-  btn_open.click(function () {
-    envelope.removeClass("close").addClass("open");
-  });
-
-  // Close on button click
-  btn_reset.click(function () {
-    envelope.removeClass("open").addClass("close");
-  });
-
-  // Also open if envelope clicked
   envelope.click(function () {
-    envelope.removeClass("close").addClass("open");
+    open();
+  });
+  btn_open.click(function () {
+    open();
+  });
+  btn_reset.click(function () {
+    close();
   });
 
+  function open() {
+    envelope.addClass("open").removeClass("close");
+  }
+  function close() {
+    envelope.addClass("close").removeClass("open");
+  }
 });
