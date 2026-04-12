d7a6e9a876aff27f3877f47495060257c76a9d4e
Vishal patil <vishalpatil120923@gmail.com>
2026-04-12 15:45:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,9 @@
-$(document).ready(function () {
-  var envelope = $("#envelope");
-  var btn_open = $("#open");
-  var btn_reset = $("#reset");
+function openEnvelope() {
+  document.getElementById("envelope").classList.remove("close");
+  document.getElementById("envelope").classList.add("open");
+}
 
-  // Open when clicking envelope
-  envelope.click(function () {
-    openEnvelope();
-  });
-
-  // Open button
-  btn_open.click(function () {
-    openEnvelope();
-  });
-
-  // Close button
-  btn_reset.click(function () {
-    closeEnvelope();
-  });
-
-  function openEnvelope() {
-    envelope.addClass("open").removeClass("close");
-
-    // Trigger hearts animation (optional class)
-    $(".heart").addClass("active");
-  }
-
-  function closeEnvelope() {
-    envelope.addClass("close").removeClass("open");
-
-    // Reset hearts animation
-    $(".heart").removeClass("active");
-  }
-});
+function closeEnvelope() {
+  document.getElementById("envelope").classList.remove("open");
+  document.getElementById("envelope").classList.add("close");
+}
