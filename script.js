6281cf48826a0f50ea0f30023d1880a9d9edf76c
Vishal patil <vishalpatil120923@gmail.com>
2026-04-12 15:51:44 +0530

    Refactor envelope functions to use jQuery

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-function openEnvelope() {
-  document.getElementById("envelope").classList.remove("close");
-  document.getElementById("envelope").classList.add("open");
-}
+$(document).ready(function () {
 
-function closeEnvelope() {
-  document.getElementById("envelope").classList.remove("open");
-  document.getElementById("envelope").classList.add("close");
-}
+  var envelope = $("#envelope");
+  var btn_open = $("#open");
+  var btn_reset = $("#reset");
+
+  // Open on button click
+  btn_open.click(function () {
+    envelope.removeClass("close").addClass("open");
+  });
+
+  // Close on button click
+  btn_reset.click(function () {
+    envelope.removeClass("open").addClass("close");
+  });
+
+  // Also open if envelope clicked
+  envelope.click(function () {
+    envelope.removeClass("close").addClass("open");
+  });
+
+});
